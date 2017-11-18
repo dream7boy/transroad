317e96475d2e063a183781fedeb4822a4791aba3
junggunlee <leejunggun7@gmail.com>
2017-11-18 11:39:32 +0900

    generate shipment model

--- app/models/shipper.rb
+++ app/models/shipper.rb
@@ -3,4 +3,6 @@ class Shipper < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :shipments, dependent: :destroy
 end
