d7d18119575a7194da2395af78e34e4774cee049
junggunlee <leejunggun7@gmail.com>
2017-11-19 13:57:41 +0900

    make form and add edit and destroy function on shipment

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,4 +1,6 @@
 class ShipmentsController < ApplicationController
+  before_action :set_shipment, only: [:show, :edit, :update, :destroy]
+
   def index
     @shipments = Shipment.all
   end
@@ -8,5 +10,40 @@ class ShipmentsController < ApplicationController
 
   def new
     @shipment = Shipment.new
+  end
+
+  def edit
+  end
+
+  def update
+    @shipment.update(shipment_params)
+    redirect_to cargos_path
+    flash[:notice] = "Your shipment has been edited"
+  end
+
+  def create
+    @shipment = Shipment.new(shipment_params)
+    @shipment.shipper = current_shipper
+    if @shipment.save
+      redirect_to shipment_path(@shipment)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @shipment.destroy
+    redirect_to cargos_path
+    flash[:notice] = "Your shipment has been deleted"
+  end
+
+  private
+
+  def shipment_params
+    params.require(:shipment).permit(:distance, :rate, :commodity, :weight, :car_type)
+  end
+
+  def set_shipment
+    @shipment = Shipment.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :shipments, only: [:index, :show, :new, :create] do
+  resources :shipments do
   end
 
   get "cargos", to: 'shippers#cargos'
