bb938981a5fa7f5c28d4650c7e982952d6c1b17b
junggunlee <leejunggun7@gmail.com>
2017-11-21 17:45:37 +0900

    edit small details on views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   devise_group :user, contains: [:shipper, :carrier]
-
-  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user! # Ensure someone is logged in
-
-  private
-
-  def storable_location?
-    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
-  end
-
-  def store_user_location!
-    store_location_for(:user, request.fullpath)
-  end
 end
