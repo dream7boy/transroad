3269e4e70f4bb394f4fb707af7d63c89dbd07abf
junggunlee <leejunggun7@gmail.com>
2018-01-15 17:25:22 +0900

    add more columns to shippers and modify new and edit page for shipper

--- app/models/shipper.rb
+++ app/models/shipper.rb
@@ -6,4 +6,6 @@ class Shipper < ApplicationRecord
 
   has_many :shipments, dependent: :destroy
   has_many :facilities, dependent: :destroy
+
+  INDUSTRIES = %w(製造 土木・建設 農業・林業・漁業 飲食 小売・卸売 印刷 商社)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115003450) do
+ActiveRecord::Schema.define(version: 20180115072816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,15 @@ ActiveRecord::Schema.define(version: 20180115003450) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "company_name"
+    t.string "post_code"
+    t.string "prefecture"
+    t.string "ward"
+    t.string "street"
+    t.string "industry"
+    t.string "name_kanji"
+    t.string "name_furigana"
+    t.string "phone"
     t.index ["email"], name: "index_shippers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_shippers_on_reset_password_token", unique: true
   end
