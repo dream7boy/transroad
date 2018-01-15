dce0687259e9aed5b8eeca70a83578476954dd45
Shun Miyaki <shunmiyaki@hotmail.com>
2018-01-16 02:47:43 +0900

    multi-select works, proceed with styling and other adj

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( jquery.jpostal.js )
-Rails.application.config.assets.precompile += %w( jquery.autoKana.js )
+Rails.application.config.assets.precompile += %w( multiple-select.js )
