a86a5ed1ff23e267ed6b09d542378635e502274d
junggunlee <leejunggun7@gmail.com>
2018-01-15 12:40:41 +0900

    edit the lst of vehicle types

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,7 +2,6 @@ class Vehicle < ApplicationRecord
   belongs_to :carrier
 
   SIZES = %w(軽貨物 小型 中型 大型 その他)
-  VEHICLE_TYPES =
-    %w(平ボディ 平ボディ（幌付き） バン バン（保冷） バン（冷凍・冷蔵）
-        ウィング ウィング（保冷） ウィング（冷凍・冷蔵） 幌ウィング)
+  VEHICLE_TYPES = %w(平ボディ 平ボディ（幌付き） バン バン（保冷） バン（冷凍・冷蔵）
+                     ウィング ウィング（保冷） ウィング（冷凍・冷蔵） 幌ウィング)
 end
