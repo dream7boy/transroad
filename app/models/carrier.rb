e3244f5aad9868f4235a334ebab4dfcfae317024
junggunlee <leejunggun7@gmail.com>
2018-01-16 22:50:35 +0900

    add codes for cocoon

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -7,7 +7,9 @@ class Carrier < ApplicationRecord
   has_many :deals, dependent: :destroy
   has_many :vehicles, dependent: :destroy
 
-  accepts_nested_attributes_for :vehicles, allow_destroy: true
+  accepts_nested_attributes_for :vehicles, allow_destroy: true, reject_if: :all_blank
 
-  FAVORITE_PRODUCTS = %w(精密機器 重量物 展示会用品 建築資材 印刷物 家具 楽器 衣料品 農産物 食料品)
+  FAVORITE_PRODUCTS =
+    %w(精密機器 重量物 展示会用品 建築資材 印刷物 家具 楽器 衣料品 農産物 食料品
+       美術品 仏壇・仏具 洋紙 遊技機)
 end
