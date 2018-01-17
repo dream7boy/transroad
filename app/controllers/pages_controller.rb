917df087652b87d3703ece9cff026c80d9fb83c7
junggunlee <leejunggun7@gmail.com>
2018-01-17 17:53:29 +0900

    users cannot go to the root anymore

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  include Accessible
   skip_before_action :authenticate_user!, only: :home
 
   def home
--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -7,7 +7,7 @@ class Carrier < ApplicationRecord
   has_many :deals, dependent: :destroy
   has_many :vehicles, dependent: :destroy
 
-  accepts_nested_attributes_for :vehicles, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :vehicles, allow_destroy: true
 
   FAVORITE_PRODUCTS =
     %w(精密機器 重量物 展示会用品 建築資材 印刷物 家具 楽器 衣料品 農産物 食料品
