0d68f61a85caac759b999be03ca9d97fe91888d5
junggunlee <leejunggun7@gmail.com>
2018-01-30 20:47:25 +0900

    make nested forms for items with cocoon gem

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -6,5 +6,6 @@ class Deal < ApplicationRecord
   validates :shipment, presence: true
   validates :carrier_id, uniqueness: { scope: [:shipment_id] }
 
+  validates :items, presence: true
   accepts_nested_attributes_for :items, allow_destroy: true
 end
