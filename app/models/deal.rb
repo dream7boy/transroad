eb04b2883c917309c772a1b52041720f4e22d38c
junggunlee <leejunggun7@gmail.com>
2018-04-10 21:14:29 +0900

    adding nested form for delivery vehilces on qutation new page.

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,10 +2,13 @@ class Deal < ApplicationRecord
   belongs_to :shipment
   belongs_to :carrier
   has_many :items, dependent: :destroy
+  has_many :delivery_vehicles, dependent: :destroy
 
   validates :shipment, presence: true
   validates :carrier_id, uniqueness: { scope: [:shipment_id] }
 
   validates :items, presence: true
+  validates :delivery_vehicles, presence: true
   accepts_nested_attributes_for :items, allow_destroy: true
+  accepts_nested_attributes_for :delivery_vehicles, allow_destroy: true
 end
