d0bd2e7d6f263a0414c8716fd94ca43f4a0cfe13
Shun Miyaki <shunmiyaki@hotmail.com>
2018-04-19 16:28:07 +0900

    added no option to the category as a default

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,7 +3,7 @@ class Vehicle < ApplicationRecord
   belongs_to :carrier, optional: true
 
   TYPES = %w(バン 平ボディ 平ボディ・幌 ウィング・アルミ ウィング・幌 クレーン車 ダンプ 重機運搬)
-  SPECIFICATIONS = %w(ワイド ロング ワイド＆ロング)
-  FEATURES = %w(パワーゲート エアサス 冷蔵・冷凍 保冷)
+  SPECIFICATIONS = %w(特になし ワイド ロング ワイド＆ロング)
+  FEATURES = %w(特になし パワーゲート エアサス パワーゲート＆エアサス 冷蔵・冷凍 保冷)
 
 end
