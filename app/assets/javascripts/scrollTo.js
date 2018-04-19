4f3eea3d6859f0ba95e666b5f3d4a5411404491a
Shun Miyaki <shunmiyaki@hotmail.com>
2018-04-19 23:11:49 +0900

    js changed from asset to local partial

--- app/assets/javascripts/scrollTo.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  const target = '#carrier_areas_covered--container div.ms-parent.form-control.grouped_select.required';
-  $(target)[0].addEventListener('click', function(e) {
-    $('html, body').animate({
-      scrollTop: $(target).offset().top
-    }, 400);
-  });
-});
