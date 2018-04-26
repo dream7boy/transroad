ead120e59533b69ef95f7e7241b5f311aedb901c
Shun Miyaki <shunmiyaki@hotmail.com>
2018-04-26 22:24:46 +0900

    Attachinary Styling on C-Register Page - Upload styling on C-Register page is styled up!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,6 +8,7 @@
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require attachinary_template
 //= require_tree .
 
 
---
+++ app/assets/javascripts/attachinary_template.js
@@ -0,0 +1,16 @@
+$.attachinary.config.template = '\
+  <ul>\
+    <% for(var i=0; i<files.length; i++){ %>\
+      <li>\
+        <% if(files[i].resource_type == "raw") { %>\
+          <div class="raw-file"></div>\
+        <% } else { %>\
+          <img\
+            src="<%= $.cloudinary.url(files[i].public_id, { "version": files[i].version, "format": "jpg", "crop": "fill", "width": 380, "height": 240 }) %>"\
+            alt="" width="190" height="120" />\
+        <% } %>\
+        <a href="#" data-remove="<%= files[i].public_id %>"><i class="md md-clear md-2x"></i></a>\
+      </li>\
+    <% } %>\
+  </ul>\
+';
