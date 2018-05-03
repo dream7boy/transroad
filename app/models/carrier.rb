544e6894aefcc1e6fa7e052b3f3bcf155588a93d
junggunlee <leejunggun7@gmail.com>
2018-05-03 12:40:03 +0900

    add validations for Carriers and Vehicles

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -7,6 +7,26 @@ class Carrier < ApplicationRecord
   has_many :deals, dependent: :destroy
   has_many :vehicles, dependent: :destroy
 
+  validates :company_name, presence: true
+  validates :post_code, presence: true
+  validates :prefecture, presence: true
+  validates :ward, presence: true
+  validates :street, presence: true
+  validates :site_url, presence: true
+  validates :ceo_name, presence: true
+  validates :founded_date, presence: true
+  validates :capital, presence: true
+  validates :employee_numbers, presence: true
+  validates :areas_covered, presence: true
+  validates :favorite_products, presence: true
+  validates :strength_1, presence: true
+  validates :strength_2, presence: true
+  validates :specialties, presence: true
+  validates :name_kanji, presence: true
+  validates :name_furigana, presence: true
+  validates :phone, presence: true
+  validates :vehicles, presence: true
+
   has_attachment :photo
 
   accepts_nested_attributes_for :vehicles, allow_destroy: true
