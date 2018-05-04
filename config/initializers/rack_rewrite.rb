459b8bb4afc5e108df1e880a548020a58ee1bd62
junggunlee <leejunggun7@gmail.com>
2018-05-05 00:42:51 +0900

    edit rack_rewrite

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,5 +1,5 @@
 if ENV['RACK_ENV'] == 'production'
-  MyAppName::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+  Transroad::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
     r301 %r{.*}, 'https://transroad.jp$&', :if => Proc.new {|rack_env|
       rack_env['SERVER_NAME'] == 'www.transroad.jp.herokudns.com'
     }
