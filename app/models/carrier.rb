08693554da10bb5242f6e1dcf86338555dbb1af9
junggunlee <leejunggun7@gmail.com>
2018-05-05 11:46:24 +0900

    add leave function for users to quit the service

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -34,4 +34,28 @@ class Carrier < ApplicationRecord
   FAVORITE_PRODUCTS =
     %w(精密機器 重量物 展示会用品 建築資材 印刷物 家具 楽器 衣料品 農産物 食料品
        美術品 仏壇・仏具 洋紙 遊技機)
+
+  def leave
+    #leave_atに退会時刻を追記
+    update_attribute(:leave_at, Time.current)
+
+    # また、userのメールアドレスの頭にleave_atを追加する。
+    # メールアドレスを変更すると原則確認メールが送信されるため、
+    # 送信をスキップすることを宣言した上でupdateする。
+    new_email = self.leave_at.to_i.to_s + '_' + self.email.to_s
+
+    # devise :confirmable した モデルには skip_confirmation! skip_reconfirmation!
+    # というメソッドが追加されるので下記のコードを追加
+    # self.skip_reconfirmation!
+
+    update_attribute(:email, new_email)
+
+    # また、social_profilesが存在する場合はuidの頭にもleave_atを追加する
+    # fb,twitter両方連携されている場合があるため、each doしている。
+    # social_profiles = self.social_profiles
+    # social_profiles.each do |sp|
+    #   new_uid = self.leave_at.to_i.to_s + '_' + sp.uid.to_s
+    #   sp.update_attribute(:uid, new_uid)
+    # end
+  end
 end
