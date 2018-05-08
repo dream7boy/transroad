d66cc82acc4f03806e9dd6af8f6e327abcb2bc06
junggunlee <leejunggun7@gmail.com>
2018-05-08 17:57:29 +0900

    add host on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   GA.tracker = ENV['GOOGLE_TRACKING_ID']
 
   # Setup the mailer config for SendGrid
+  config.action_mailer.default_url_options = { host: "www.transroad.jp" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
