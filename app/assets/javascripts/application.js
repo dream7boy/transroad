8b93768f6190322af6e82a7eabba72bf615213b5
Shun Miyaki <shunmiyaki@hotmail.com>
2018-05-22 18:23:16 +0900

    Landing & Results - Landing Page Complete - Results Page Complete - Full checks will be done after registration page is done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,39 @@
 //= require_tree .
 
 
+
 // AUTO CLEAR ALERT AFTER X SECONDS
 $('document').ready(function() {
   setTimeout(function() {
     $('.alert').slideUp();
   }, 3000);
 });
+
+
+
+// POLYFILL FOR .INCLUDES()
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
+
+
+
+// POLYFILL FOR .FOREACH()
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
